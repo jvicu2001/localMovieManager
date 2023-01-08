0aa07fbdf0b97c5ddd65ee633feaf9d5b5e34518
Jorge Vicuña <jvicuna2001@gmail.com>
2023-01-07 22:41:38 -0300

    Fix pagination logic

--- frontend/src/Pagination.svelte
+++ frontend/src/Pagination.svelte
@@ -10,6 +10,7 @@
 </script>
 
 <div class="flex justify-center">
+    <p>Current Page: {currentPage}</p>
     {#if currentPage > 1}
         <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, 1)}>
             <Icon icon="material-symbols:first-page" class="mx-auto"/>
@@ -18,23 +19,28 @@
             <Icon icon="material-symbols:chevron-left" class="mx-auto"/>
         </button>
     {/if}
+
+    <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, 1)}>1</button>
     
     {#if totalPages > 1}
         {#if (currentPage - sidePages) > 1}
-            <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, 1)}>1</button>
+            
             <div class="text-white ring-2 rounded-md w-10 p-2">...</div>
         {/if}
         {#each Array(totalPages) as page, i}
-            {#if (i + 1) > (currentPage - sidePages) && (i + 1) < (currentPage + sidePages)}
+            {#if (i + 1) > (currentPage - sidePages) && (i + 1) < (currentPage + sidePages) && ((i+1) !== 1) && ((i+1) !== totalPages)}
                 <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, i+1)}>{i + 1}</button>
             {/if}
         {/each}
         {#if (currentPage + sidePages) < totalPages}
             <div class="text-white ring-2 rounded-md w-10 p-2">...</div>
-            <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, totalPages)}>{totalPages}</button>
+            
         {/if} 
+
+        <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, totalPages)}>{totalPages}</button>
     {/if}
 
+    
         
     {#if currentPage < totalPages}
         <button class="text-white ring-2 rounded-md w-10 p-2" on:click={() => searchMovies(searchText, currentPage + 1)}>
